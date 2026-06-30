deaacd939c3f514d907674f900f85f24f10f5606
santhosh220z <santhoshsunkarasbe@gmail.com>
2026-06-30 22:38:04 +0530

    my initial commit on my first day progress on learn java in 30 days.

---
+++ first.java
@@ -0,0 +1,13 @@
+public class Main {
+    //learning Java for the first time 
+    /*
+    DAY_1
+    for day one i covered the following topics 
+    1.introduction tio java
+    2.basic syntax
+    3.datatypes
+    */
+    public static void main(String[] args){
+        System.out.println("Hello World");
+    }
+}
