9d49867aa862ecd60da04a58e564bede3734fe56
santhosh220z <santhoshsunkarasbe@gmail.com>
2026-07-01 22:05:25 +0530

    learned about datatypes and printed them

--- first.java
+++ first.java
@@ -9,5 +9,35 @@ public class Main {
     */
     public static void main(String[] args){
         System.out.println("Hello World");
+
+    
+    /*
+    DAY_2 
+    for day to i covered the following topics:
+    1. variables
+    2. datatypes
+    3. inputs
+    4. if else statements
+    */
+
+    int age = 21; //integer variable is used to store whole numbers and is represented by int
+    String carName = "Konigsegg Jesko"; //string variable os used to store text and is represented by double quotes
+    String name = "santhosh"; //string variable is used to store text and is represented by double quotes
+    double marks = 91.3; //double variable is used to store decimal numbers and is represented by d at the end of the number
+    float temperature = 36.6f; //float variable is used to store decimal numbers and is represented by f at the end of the number
+    long price = 200000000l; //long variable is used to store large numbers
+    char grade = 'A'; //character variable is a single character and is represented by single quotes
+    boolean isAdult = true; //boolean variable it only has 2 states true or false
+    boolean isStudent = false;
+
+    System.out.println(name);
+    System.out.println(age);
+    System.out.println(carName);
+    System.out.println(marks);
+    System.out.println(temperature);
+    System.out.println(price);
+    System.out.println(grade);
+    System.out.println(isAdult);
+    System.out.println(isStudent);
     }
 }
